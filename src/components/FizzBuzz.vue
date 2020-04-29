f97519d32bba37758898cba4b3666a0e6d50b545
Florian Schildwaechter <florian.schildwaechter@bauerxcel.de>
2020-04-29 23:08:03 +0200

    git going

---
+++ src/components/FizzBuzz.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="fizzbuzz">
+    <p>Gib eine Zahl ein und versuch dein Glück :)</p>
+    <h1 v-if="error.length">{{error}}</h1>
+    <input v-model="input" type="number" />
+    <button v-on:click="validate">submit</button>
+    {{ output }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FizzBuzz',
+  data () {
+    return {
+      input: 0,
+      output: '',
+      error: ''
+    }
+  },
+  methods: {
+    validate () {
+      if (this.input <= 0) {
+        this.error = 'Deine Eingabe muss eine Zahl sein die größer als 0 ist!'
+      }
+      this.fizzBuzz()
+    },
+    fizzBuzz () {
+      const i = this.input
+      this.output =
+        i % 3 === 0 && i % 5 === 0
+          ? 'fizzbuzz'
+          : i % 5 === 0
+            ? 'buzz'
+            : i % 3 === 0
+              ? 'fizz'
+              : i
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <fizz-buzz></fizz-buzz>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import FizzBuzz from '@/components/FizzBuzz.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    FizzBuzz
   }
 }
 </script>
