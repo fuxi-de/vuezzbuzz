bb98c49c3f53d7fb1094062f22b5a1f035cca1d4
Florian Schildwaechter <florian.schildwaechter@bauerxcel.de>
2020-04-30 16:14:43 +0200

    chore: some cleanup

--- src/components/FizzBuzz.vue
+++ src/components/FizzBuzz.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="fizzbuzz flex flex-col w-9/12 m-auto">
-    <span class="output">{{ output }}</span>
-    <p class="mb-16">Gib eine Zahl ein und versuch dein Glück :)</p>
+  <div class="fizzbuzz flex flex-col w-9/12 m-auto h-screen">
     <div
       class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative w-full max-w-sm m-auto"
       role="alert"
@@ -23,6 +21,7 @@
         </svg>
       </span>
     </div>
+
     <form class="w-full max-w-sm m-auto">
       <div class="flex items-center border-b border-b-2 border-teal-500 py-2">
         <input
@@ -34,18 +33,46 @@
         <button
           class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
           type="button"
-          v-on:click="validate"
+          @click="validate"
         >
           Fizz the Buzz
         </button>
         <button
           class="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"
           type="button"
+          name="input"
+          @click="reset($event)"
         >
           Cancel
         </button>
       </div>
     </form>
+
+    <div
+      class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md mt-16 w-full max-w-sm m-auto"
+      role="alert"
+      v-if="output.length"
+    >
+      <div class="flex">
+        <div class="py-1">
+          <svg
+            class="fill-current h-6 w-6 text-teal-500 mr-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"
+            />
+          </svg>
+        </div>
+        <div>
+          <p class="font-bold">Fizzle da buzz:</p>
+          <p class="text-sm">
+            {{ output }}
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -54,7 +81,7 @@ export default {
   name: 'FizzBuzz',
   data () {
     return {
-      input: 0,
+      input: '',
       output: '',
       error: ''
     }
@@ -62,10 +89,16 @@ export default {
   methods: {
     validate () {
       this.error = ''
+      this.output = ''
       if (this.input <= 0) {
         this.error = 'Deine Eingabe muss eine Zahl sein die größer als 0 ist!'
+        return
       }
       this.fizzBuzz()
+    },
+    reset () {
+      this.input = 0
+      this.error = ''
     },
     fizzBuzz () {
       const i = this.input
